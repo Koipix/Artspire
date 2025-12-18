11aa5d4ad9323c22186dbe795b3e5b576602a70c
gianren17solana <rengian17solana@gmail.com>
2025-12-19 02:41:29 +0800

    Added new font and text header

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -5,6 +5,19 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: const Color(0xFF21212e));
+    return Scaffold(
+      backgroundColor: const Color(0xFF21212e),
+      body: Text(
+        "For the love of human creativity",
+        style: TextStyle(
+          fontFamily: 'Poppins',
+          fontSize: 80,
+          fontWeight: FontWeight.w700,
+          letterSpacing: 1,
+          height: 1.2,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
