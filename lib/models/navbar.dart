79796428eeaa1eca755650e197c35e0c3b9a787c
Koipix <koi.devt@gmail.com>
2025-12-19 15:09:15 +0800

    refac: navbar model

---
+++ lib/models/navbar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      destinations: [
+        NavigationDestination(
+          icon: SvgPicture.asset(
+            "assets/icons/Home_Highlight.svg",
+            height: 55,
+            width: 55,
+          ), 
+          label: "uwu",
+        ),
+        NavigationDestination(
+          icon: SvgPicture.asset(
+            "assets/icons/Compass.svg",
+            height: 25,
+            width: 25,
+          ), 
+          label: "uwu",
+        ),
+        NavigationDestination(
+          icon: SvgPicture.asset(
+            "assets/icons/Add.svg",
+            height: 50,
+            width: 50,
+          ), 
+          label: "uwu",
+        ),
+        NavigationDestination(
+          icon: SvgPicture.asset(
+            "assets/icons/Bell.svg",
+            height: 25,
+            width: 25,
+          ), 
+          label: "uwu",
+        ),
+        NavigationDestination(
+          icon: SvgPicture.asset(
+            "assets/icons/User.svg",
+            height: 25,
+            width: 25,
+          ), 
+          label: "uwu",
+        ),
+      ],
+      labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+      backgroundColor: const Color(0xFF21212e),
+      indicatorColor: Colors.transparent,
+      height: 70,
+    );
+  }
+}
