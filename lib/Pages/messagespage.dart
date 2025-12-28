72dba24506d10170d81de8f6d1f52c2f97077712
gianren17solana <rengian17solana@gmail.com>
2025-12-29 06:11:08 +0800

    fixed class searchbar wd chattype

--- lib/Pages/messagespage.dart
+++ lib/Pages/messagespage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:artspire/models/searcbar.dart';
 
 class MessagesPage extends StatelessWidget {
   const MessagesPage({super.key});
@@ -11,12 +12,7 @@ class MessagesPage extends StatelessWidget {
       backgroundColor: const Color(0xFF21212E),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          MessageHeader(),
-          SearchDM(),
-          // ChatType(),
-          // ChatTree()
-        ],
+        children: const [MessageHeader(), ChatType(), ChatTree()],
       ),
     );
   }
@@ -29,50 +25,70 @@ class MessageHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(top: 42),
-      child: Center(
-        child: Text(
-          "Messages",
-          style: GoogleFonts.poppins(fontSize: 28, color: Colors.white),
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Messages",
+            style: GoogleFonts.poppins(fontSize: 28, color: Colors.white),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14),
+            child: Searchbar(),
+          ),
+        ],
       ),
     );
   }
 }
 
-class SearchDM extends StatelessWidget {
-  const SearchDM({super.key});
+class ChatType extends StatelessWidget {
+  const ChatType({super.key});
+
+  static const List<String> msgcategs = ["All", "My Requests"];
+  static const int selectedcateg = 0;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 25, left: 20, right: 20),
-      child: SearchBar(
-        leading: SvgPicture.asset(
-          "assets/icons/Search.svg",
-          height: 22,
-          width: 22,
-        ),
-        hintText: "Search direct messages",
-        hintStyle: MaterialStateProperty.all(
-          GoogleFonts.poppins(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            color: const Color(0xFFC5C2D2),
-          ),
-        ),
-        textStyle: MaterialStateProperty.all(
-          GoogleFonts.poppins(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: WidgetStateProperty.all(const Color(0xFF383843)),
-        elevation: WidgetStateProperty.all(0),
-        padding: WidgetStatePropertyAll(
-          EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-        ),
+      margin: const EdgeInsets.only(top: 20, left: 25, right: 25),
+      child: Row(
+        children: List.generate(msgcategs.length, (index) {
+          final isSelected = index == selectedcateg;
+
+          return Padding(
+            padding: EdgeInsets.only(
+              right: index == msgcategs.length - 1 ? 0 : 10,
+            ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 3),
+              decoration: isSelected
+                  ? BoxDecoration(
+                      color: const Color(0xFF7A88F2),
+                      borderRadius: BorderRadius.circular(8),
+                    )
+                  : BoxDecoration(borderRadius: BorderRadius.circular(8)),
+              child: Text(
+                msgcategs[index],
+                style: GoogleFonts.poppins(
+                  fontSize: isSelected ? 14 : 13,
+                  fontWeight: isSelected ? FontWeight.w500 : FontWeight.normal,
+                  color: isSelected ? Colors.white : const Color(0xFFC5C2D2),
+                ),
+              ),
+            ),
+          );
+        }),
       ),
     );
+  }
+}
+
+class ChatTree extends StatelessWidget {
+  const ChatTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
