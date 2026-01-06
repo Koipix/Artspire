b793f4a1ba527271bb7f0e20f9e14bd5f120e872
Koipix <koi.devt@gmail.com>
2026-01-07 01:36:48 +0800

    card_detail data fetching

--- flutter/lib/screens/searchpage_card.dart
+++ flutter/lib/screens/searchpage_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:artspire/models/searchItem.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:artspire/models/searchItem.dart';
 import 'package:artspire/data_rep.dart';
 import 'package:artspire/models/searchItem.dart';
 
@@ -23,24 +24,36 @@ class SearchCardDetails extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFF21212E),
       appBar: AppBar(
-        title: CardHeader(
-          item: getItem(),
-        ),
         centerTitle: true,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/XButton.svg"
+          ),
+          onPressed: () {
+            Navigator.of(context).maybePop();
+          },
+        ) 
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          HeaderImage(
+            item: getItem(),
+          ),
+          HeaderDetails(
+            item: getItem(),
+          ),
         ],
       ),
     );
   }
 }
 
-class CardHeader extends StatelessWidget {
+class HeaderImage extends StatelessWidget {
   final SearchItem? item;
 
-  const CardHeader({
+  const HeaderImage({
     super.key,
     required this.item,
   });
@@ -48,13 +61,73 @@ class CardHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(
-        item?.cardName ?? 'Item not found',
-        style: GoogleFonts.poppins(
-          fontSize: 28, 
-          color: Colors.black
+      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      child: AspectRatio(
+        aspectRatio: 4 / 3,
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            image: item?.imgPath.isNotEmpty ?? false
+            ? DecorationImage(
+              image: AssetImage(item!.imgPath),
+              fit: BoxFit.cover
+            ) : null, 
+          ), 
         ),
       ),
+    ); 
+  }
+}
+
+class HeaderDetails extends StatelessWidget {
+  final SearchItem? item;
+
+  const HeaderDetails({
+    super.key,
+    required this.item,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                item?.cardName ?? 'Item not found',
+                style: GoogleFonts.poppins(
+                  fontSize: 22, 
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CircleAvatar(
+                    radius: 10,
+                    backgroundImage: AssetImage(item?.pImgPath ?? ""),
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    item?.artistName ?? '',
+                    style: GoogleFonts.poppins(
+                      fontSize: 14, 
+                      fontWeight: FontWeight.w400,
+                      color: const Color(0xFFC5C2D2),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ) 
     );
   }
 }
