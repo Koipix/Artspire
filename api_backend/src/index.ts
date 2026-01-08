61dedcbebc6300e8acc4eb64d498b778a08df9fc
Koipix <koi.devt@gmail.com>
2026-01-08 15:32:29 +0800

    allow cors for testing

--- api_backend/src/index.ts
+++ api_backend/src/index.ts
@@ -1,4 +1,5 @@
 import server, { getItems, getItemById } from "./server"
+import cors from 'cors';
 import { Router } from 'express';
 
 const router = Router();
@@ -7,8 +8,9 @@ const port = 3000;
 router.get("/fetchItem/:id", getItemById);
 router.get('/fetchItem', getItems);
 
+server.use(cors()); //TODO: remove in production, only for testing
 server.use(router);
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
   console.log("Server is running...")
 });
