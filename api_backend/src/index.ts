a2e4c84e46fb7e415a4364a317fa7b169570caea
Koipix <koi.devt@gmail.com>
2026-01-09 08:17:12 +0800

    server stripe setup

--- api_backend/src/index.ts
+++ api_backend/src/index.ts
@@ -1,4 +1,5 @@
-import server, { getItems, getItemById } from "./server"
+import server, { getItems, getItemById, processPayment } from "./server"
+import express from 'express';
 import cors from 'cors';
 import { Router } from 'express';
 
@@ -7,9 +8,12 @@ const port = 3000;
 
 router.get("/fetchItem/:id", getItemById);
 router.get('/fetchItem', getItems);
+router.post('/api/payment', processPayment);
 
 server.use(cors()); //TODO: remove in production, only for testing
 server.use(router);
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
 
 server.listen(port, '0.0.0.0', () => {
   console.log("Server is running...")
